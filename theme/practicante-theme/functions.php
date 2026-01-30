8db3d8266814362c7d629b2ca67bafe012b21ee0
angel mejia <angelmejiaprz@gmail.com>
2026-01-30 15:20:56 -0600

    add theme supports and menu

---
+++ theme/practicante-theme/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function practicante_theme_setup() {
+    add_theme_support('post-thumbnails');
+
+    register_nav_menus([
+        'main_menu' => 'Menú Principal'
+    ]);
+}
+
+add_action('after_setup_theme', 'practicante_theme_setup');
