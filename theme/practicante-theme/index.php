8e69a412c911380bf0b625937cc16dabb0e905ae
angel mejia <angelmejiaprz@gmail.com>
2026-01-30 16:31:05 -0600

    add basic index loop

---
+++ theme/practicante-theme/index.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<main>
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+            <article>
+                <h2><?php the_title(); ?></h2>
+                <?php the_content(); ?>
+            </article>
+        <?php endwhile; ?>
+    <?php else : ?>
+        <p>No hay contenido para mostrar.</p>
+    <?php endif; ?>
+</main>
+
+<?php get_footer(); ?>
